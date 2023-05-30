a8f9cf60be0272793990d1edb5e14d1ba5b0a96f
Merlin Roe <merlin.roe@hotmail.co.uk>
2023-05-30 14:55:10 +0100

    Clean up and Attribute renames

--- hexital/types/indicator.py
+++ hexital/types/indicator.py
@@ -1,42 +1,37 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
+from typing import List, Optional
 
 from hexital.types.ohlcv import Candle
 
 
 @dataclass(kw_only=True)
 class Indicator(ABC):
-    candles: List[Candle]
+    candles: List[Candle] = field(default_factory=list)
     indicator_name: str = None
     override_name: str = None
-    _round_value: int = 4
-    _output_indicator_name: str = ""
+    round_value: int = 4
+    _output_name: str = ""
 
     def __post_init__(self):
-        self._output_indicator_name = (
+        self._output_name = (
             self.override_name if self.override_name else self._generate_name()
         )
         self.calculate()
 
-    @property
-    def name(self):
-        """The indicator name that will be saved into the Candles"""
-        return self._output_indicator_name
-
-    @property
-    def round_by(self):
-        return self._round_value
-
-    @round_by.setter
-    def round_by(self, value):
-        self._round_value = value
+    def _initialise(self):
+        pass
 
     @abstractmethod
     def _generate_name(self):
         pass
+
+    @property
+    def name(self):
+        """The indicator name that will be saved into the Candles"""
+        return self._output_name
 
     @abstractmethod
     def _calculate_new_value(self, index: int = -1) -> float | None:
@@ -46,41 +41,44 @@ class Indicator(ABC):
         """Calculate the TA values, will calculate for all the Candles,
         where this indicator is missing"""
         for index in range(self._find_starting_index(), len(self.candles)):
-            if self._output_indicator_name not in self.candles[index].hex_ta:
+            if self._output_name not in self.candles[index].hex_ta:
                 value = self._calculate_new_value(index=index)
-
                 if value:
-                    value = round(value, self._round_value)
+                    value = round(value, self.round_value)
 
-                self.candles[index].hex_ta[self._output_indicator_name] = value
+                self.candles[index].hex_ta[self._output_name] = value
 
     def _find_starting_index(self) -> int:
         """Optimisation method, to find where to start calculating the indicator from"""
-        if self._output_indicator_name not in self.candles[0].hex_ta:
+        if self._output_name not in self.candles[0].hex_ta:
             return 0
 
         for index in range(len(self.candles) - 1, 0, -1):
-            if self._output_indicator_name in self.candles[index].hex_ta:
+            if self._output_name in self.candles[index].hex_ta:
                 return index + 1
         return 0
 
+    # def add_sub_indicator(self, indicator: Indicator) -> None:
+    #     self._sub_indicators.append(indicator)
+
     def get_newest(self) -> float | dict:
         """Get's this newest value of this indicator"""
-        return self.candles[-1].ta.get(self._output_indicator_name, None)
+        return self.candles[-1].ta.get(self._output_name, None)
 
     def get_prev(self, index: int = None) -> float | dict:
         """Get's this Prev value of this indicator.
         if no index is specified gets prev latest"""
         if index is None:
             index = len(self.candles) - 1
-        return self.get_indicator(self.candles[index - 1], self._output_indicator_name)
+
+        return self.get_indicator(self.candles[index - 1], self._output_name)
 
     def get_indicator(
         self, candle: Candle, name: str = None, default=None
     ) -> float | dict:
         """Simple method to an indicator from a candle, regardless of it's location"""
         if not name:
-            name = self._output_indicator_name
+            name = self._output_name
         try:
             return getattr(candle, name)
         except AttributeError:
@@ -88,9 +86,9 @@ class Indicator(ABC):
 
     def get_as_list(self) -> List[float | dict]:
         """Gathers the indicator for all candles as a list"""
-        return [candle.hex_ta.get(self._output_indicator_name) for candle in self.candles]
+        return [candle.hex_ta.get(self._output_name) for candle in self.candles]
 
     @property
     def has_output_value(self) -> bool:
         """Simple boolean to state if values are being generated yet in the candles"""
-        return self.candles[-1].hex_ta.get(self._output_indicator_name) is not None
+        return self.candles[-1].hex_ta.get(self._output_name) is not None
---
+++ tests/types/test_internal.py
@@ -0,0 +1,99 @@
+import pytest
+from hexital import EMA, Candle
+
+
+@pytest.fixture(name="candles")
+@pytest.mark.usefixtures("nasdaq_candles")
+def fixture_nasdaq_data(nasdaq_candles):
+    return Candle.from_dicts(nasdaq_candles)
+
+
+@pytest.fixture(name="nasdaq_candles_30")
+def fixture_nasdaq_data_30(candles):
+    return candles[0:30]
+
+
+@pytest.fixture(name="nasdaq_candles_31st")
+def fixture_nasdaq_data_31st(candles):
+    return candles[30]
+
+
+def test_data(nasdaq_candles_30):
+    expected = [
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        11817.1465,
+        11842.3125,
+        11856.8027,
+        11900.6215,
+        11911.2985,
+        11925.591,
+        11949.3662,
+        11962.4046,
+        11942.5785,
+        11885.1919,
+        11782.7194,
+    ]
+
+    test = EMA(candles=nasdaq_candles_30, period=20, input_value="close")
+
+    assert test.get_as_list() == expected
+
+
+def test_data_append(nasdaq_candles_30, nasdaq_candles_31st):
+    expected = [
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        11817.1465,
+        11842.3125,
+        11856.8027,
+        11900.6215,
+        11911.2985,
+        11925.591,
+        11949.3662,
+        11962.4046,
+        11942.5785,
+        11885.1919,
+        11782.7194,
+        11691.827,
+    ]
+
+    test = EMA(candles=nasdaq_candles_30, period=20, input_value="close")
+    nasdaq_candles_30.append(nasdaq_candles_31st)
+    test.calculate()
+
+    assert test.get_as_list() == expected
