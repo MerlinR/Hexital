966a97cea2e08312a61d08f8fcd1c3d9f9bab1bb
Merlin Roe <merlin.roe@hotmail.co.uk>
2023-09-20 11:23:02 +0100

    Added round_values unit tests

--- tests/lib/test_utils.py
+++ tests/lib/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from hexital.lib.utils import candles_sum
+from hexital.lib.utils import candles_sum, round_values
 
 
 def test_candle_sum_reg_close(minimal_candles):
@@ -24,3 +24,26 @@ def test_candle_sum_reg_indicator_insane_index(minimal_candles):
 
 def test_candle_sum_reg_indicator_insane_index_and_length(minimal_candles):
     assert candles_sum(minimal_candles, "ATR", length=100, index=100) == 21000
+
+
+def test_round_values_simple():
+    assert round_values(100.696666) == 100.6967
+
+
+def test_round_values_simple_int():
+    assert round_values(100) == 100
+
+
+def test_round_values_none():
+    assert round_values(None) is None
+
+
+def test_round_values_dict_simple():
+    assert round_values({"one": 100.696666}) == {"one": 100.6967}
+
+
+def test_round_values_dict_mixed():
+    assert round_values({"one": 100.696666, "nada": None}) == {
+        "one": 100.6967,
+        "nada": None,
+    }
