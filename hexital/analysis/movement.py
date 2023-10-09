20ea8f972cba9dca9c04fce472a51ee8d9111ce8
Merlin Roe <merlin.roe@hotmail.co.uk>
2023-10-09 14:34:58 +0100

    Fixed movement methods to have proper validation and checks

--- hexital/analysis/movement.py
+++ hexital/analysis/movement.py
@@ -7,35 +7,47 @@ from hexital.lib.candle_extension import (
     reading_count,
     reading_period,
 )
+from hexital.lib.utils import absindex, valid_index
 
 
 def positive(candles: Union[Candle, List[Candle]], position: int = -1) -> bool:
+    if not candles:
+        return False
     if isinstance(candles, list):
-        if position >= len(candles):
-            position = -1
+        if not valid_index(position, len(candles)):
+            return False
         return candles[position].open < candles[position].close
     return candles.open < candles.close
 
 
 def negative(candles: Union[Candle, List[Candle]], position: int = -1) -> bool:
+    if not candles:
+        return False
     if isinstance(candles, list):
-        if position >= len(candles):
-            position = -1
+        if not valid_index(position, len(candles)):
+            return False
         return candles[position].open > candles[position].close
     return candles.open > candles.close
 
 
-def value_range(candles: List[Candle], indicator: str, length: int = 4) -> bool:
+def value_range(candles: List[Candle], indicator: str, length: int = 4) -> float:
     """Returns the difference between the min and max values in a indicator series.
-    Length `includes` latest"""
+    Length `includes` latest, if lenth is too long for amount of candles,
+    will check all of them"""
+    end_index = absindex(-1, len(candles))
+    if end_index is None:
+        return None
+
     if not reading_period(candles, length, indicator):
-        length = reading_count(candles, indicator)
+        length = reading_count(candles, indicator) - 1
+
+    if length < 2:
+        return None
 
     readings = [
         reading_by_candle(candle, indicator)
-        for candle in candles[-abs(length + 1) : len(candles)]
+        for candle in candles[end_index - length : end_index + 1]
     ]
-
     return abs(min(readings) - max(readings))
 
 
@@ -43,8 +55,15 @@ def rising(candles: List[Candle], indicator: str, length: int = 4) -> bool:
     """True if current `indicator` is greater than any previous `indicator`
     for `length` bars back, False otherwise.
     Length `excludes` latest"""
+    end_index = absindex(-1, len(candles))
+    if end_index is None:
+        return False
+
     if not reading_period(candles, length, indicator):
-        length = reading_count(candles, indicator)
+        length = reading_count(candles, indicator) - 1
+
+    if length < 1 or len(candles) < 2:
+        return False
 
     newest_reading = reading_by_candle(candles[-1], indicator)
 
@@ -52,7 +71,7 @@ def rising(candles: List[Candle], indicator: str, length: int = 4) -> bool:
         val < newest_reading
         for val in [
             reading_by_candle(candle, indicator)
-            for candle in candles[-abs(length + 1) : -1]
+            for candle in candles[end_index - length : end_index]
         ]
         if val is not None
     )
@@ -62,8 +81,15 @@ def falling(candles: List[Candle], indicator: str, length: int = 4) -> bool:
     """True if current `indicator` reading is less than any previous `indicator`
     reading for `length` bars back, False otherwise.
     Length `excludes` latest"""
+    end_index = absindex(-1, len(candles))
+    if end_index is None:
+        return False
+
     if not reading_period(candles, length, indicator):
-        length = reading_count(candles, indicator)
+        length = reading_count(candles, indicator) - 1
+
+    if length < 1 or len(candles) < 2:
+        return False
 
     newest_reading = reading_by_candle(candles[-1], indicator)
 
@@ -71,7 +97,7 @@ def falling(candles: List[Candle], indicator: str, length: int = 4) -> bool:
         val > newest_reading
         for val in [
             reading_by_candle(candle, indicator)
-            for candle in candles[-abs(length + 1) : -1]
+            for candle in candles[end_index - length : end_index]
         ]
         if val is not None
     )
@@ -83,13 +109,20 @@ def mean_rising(candles: List[Candle], indicator: str, length: int = 4) -> bool:
 
     Calc:
         NewestCandle[indicator] > mean(Candles[newest] to Candles[length])"""
+    end_index = absindex(-1, len(candles))
+    if end_index is None:
+        return False
+
     if not reading_period(candles, length, indicator):
-        length = reading_count(candles, indicator)
+        length = reading_count(candles, indicator) - 1
+
+    if length < 1 or len(candles) < 2:
+        return False
 
     mean = (
         sum(
             reading_by_candle(candle, indicator)
-            for candle in candles[-abs(length + 1) : -1]
+            for candle in candles[end_index - length : end_index]
         )
         / length
     )
@@ -102,13 +135,20 @@ def mean_falling(candles: List[Candle], indicator: str, length: int = 4) -> bool
 
     Calc:
         NewestCandle[indicator] > mean(Candles[newest] to Candles[length])"""
+    end_index = absindex(-1, len(candles))
+    if end_index is None:
+        return False
+
     if not reading_period(candles, length, indicator):
-        length = reading_count(candles, indicator)
+        length = reading_count(candles, indicator) - 1
+
+    if length < 1 or len(candles) < 2:
+        return False
 
     mean = (
         sum(
             reading_by_candle(candle, indicator)
-            for candle in candles[-abs(length + 1) : -1]
+            for candle in candles[end_index - length : end_index]
         )
         / length
     )
@@ -116,97 +156,114 @@ def mean_falling(candles: List[Candle], indicator: str, length: int = 4) -> bool
 
 
 def highest(candles: List[Candle], indicator: str, length: int = 4) -> float:
-    """Highest reading for a given number of bars back. Length `Includes` latest.
+    """Highest reading for a given number of bars back.
     Returns:
         Highest reading in the series.
     """
     if not reading_period(candles, length, indicator):
         length = reading_count(candles, indicator)
 
-    return max(reading_by_candle(candle, indicator) for candle in candles[-abs(length) :])
+    return max(
+        (reading_by_candle(candle, indicator) for candle in candles[-length:]),
+        default=False,
+    )
 
 
 def lowest(candles: List[Candle], indicator: str, length: int = 4) -> float:
-    """Lowest reading for a given number of bars back. Length `Includes` latest.
+    """Lowest reading for a given number of bars back.
     Returns:
         Lowest reading in the series.
     """
     if not reading_period(candles, length, indicator):
         length = reading_count(candles, indicator)
 
-    return min(reading_by_candle(candle, indicator) for candle in candles[-abs(length) :])
+    return min(
+        (reading_by_candle(candle, indicator) for candle in candles[-length:]),
+        default=False,
+    )
 
 
 def highestbar(candles: List[Candle], indicator: str, length: int = 4) -> int:
-    """Highest reading offset for a given number of bars back. Length `Excludes` latest.
+    """Highest reading offset for a given number of bars back.
     Returns:
         Offset to the lowest bar
     """
+    start_index = absindex(-1, len(candles))
+    if start_index is None:
+        return None
+
     if not reading_period(candles, length, indicator):
         length = reading_count(candles, indicator)
 
     high = None
     distance = 0
-    start_index = len(candles) - 2
 
     for dist, index in enumerate(range(start_index, start_index - length, -1)):
         current = reading_by_index(candles, indicator, index)
-
         if high and high < current:
             high = current
             distance = dist
         elif high is None:
             high = current
 
-    return distance + 1
+    return distance
 
 
 def lowestbar(candles: List[Candle], indicator: str, length: int = 4) -> int:
-    """Lowest reading offset for a given number of bars back. Length `Excludes` latest.
+    """Lowest reading offset for a given number of bars back.
     Returns:
         Offset to the lowest bar
     """
+    start_index = absindex(-1, len(candles))
+    if start_index is None:
+        return None
+
     if not reading_period(candles, length, indicator):
         length = reading_count(candles, indicator)
 
     low = None
     distance = 0
-    start_index = len(candles) - 2
 
     for dist, index in enumerate(range(start_index, start_index - length, -1)):
         current = reading_by_index(candles, indicator, index)
-
         if low and low > current:
             low = current
             distance = dist
         elif low is None:
             low = current
 
-    return distance + 1
+    return distance
 
 
 def cross(
     candles: List[Candle], indicator_one: str, indicator_two: str, length: int = 1
 ) -> bool:
-    """The `indicator_one` reading is defined as having crossed `indicator_two` reading"""
+    """The `indicator_one` reading is defined as having crossed `indicator_two` reading.
+    Either direction"""
+    start_index = absindex(-1, len(candles))
+    if start_index is None:
+        return False
+
     if not reading_period(candles, length, indicator_one) or not reading_period(
         candles, length, indicator_two
     ):
         length = min(
             [reading_count(candles, indicator_one), reading_count(candles, indicator_two)]
         )
-
-    over = reading_by_index(candles, indicator_one) > reading_by_index(
-        candles, indicator_two
-    )
-
-    start_index = len(candles) - 2
+        length -= 1
 
     for index in range(start_index, start_index - length, -1):
         if (
             reading_by_index(candles, indicator_one, index)
             > reading_by_index(candles, indicator_two, index)
-        ) != over:
+            and reading_by_index(candles, indicator_one, index - 1)
+            <= reading_by_index(candles, indicator_two, index - 1)
+        ) or (
+            reading_by_index(candles, indicator_one, index)
+            < reading_by_index(candles, indicator_two, index)
+            and reading_by_index(candles, indicator_one, index - 1)
+            >= reading_by_index(candles, indicator_two, index - 1)
+        ):
             return True
 
     return False
@@ -216,24 +273,25 @@ def crossover(
     candles: List[Candle], indicator_one: str, indicator_two: str, length: int = 1
 ) -> bool:
     """The `indicator_one` reading is defined as having crossed over `indicator_two` reading,
-    If  `indicator_two` is higher then `indicator_one` and in the last `length` it was under"""
+    If  `indicator_two` is higher then `indicator_one` and in the last `length` it was under.
+    Length is how far back to check, if length is greater then amount of candles, check all"""
+    start_index = absindex(-1, len(candles))
+    if start_index is None:
+        return False
+
     if not reading_period(candles, length, indicator_one) or not reading_period(
         candles, length, indicator_two
     ):
         length = min(
             [reading_count(candles, indicator_one), reading_count(candles, indicator_two)]
         )
-
-    if not reading_by_index(candles, indicator_one) > reading_by_index(
-        candles, indicator_two
-    ):
-        return False
-
-    start_index = len(candles) - 2
+        length -= 1
 
     for index in range(start_index, start_index - length, -1):
-        if reading_by_index(candles, indicator_one, index) < reading_by_index(
+        if reading_by_index(candles, indicator_one, index) > reading_by_index(
             candles, indicator_two, index
+        ) and reading_by_index(candles, indicator_one, index - 1) <= reading_by_index(
+            candles, indicator_two, index - 1
         ):
             return True
 
@@ -244,24 +302,25 @@ def crossunder(
     candles: List[Candle], indicator_one: str, indicator_two: str, length: int = 1
 ) -> bool:
     """The `indicator_one` reading is defined as having crossed under `indicator_two` reading,
-    If  `indicator_two` is lower then `indicator_one` and in the last `length` it was over"""
+    If `indicator_two` is lower then `indicator_one` and in the last `length` it was over.
+    Length is how far back to check, if length is greater then amount of candles, check all"""
+    start_index = absindex(-1, len(candles))
+    if start_index is None:
+        return False
+
     if not reading_period(candles, length, indicator_one) or not reading_period(
         candles, length, indicator_two
     ):
         length = min(
             [reading_count(candles, indicator_one), reading_count(candles, indicator_two)]
         )
-
-    if not reading_by_index(candles, indicator_one) < reading_by_index(
-        candles, indicator_two
-    ):
-        return False
-
-    start_index = len(candles) - 2
+        length -= 1
 
     for index in range(start_index, start_index - length, -1):
-        if reading_by_index(candles, indicator_one, index) > reading_by_index(
+        if reading_by_index(candles, indicator_one, index) < reading_by_index(
             candles, indicator_two, index
+        ) and reading_by_index(candles, indicator_one, index - 1) >= reading_by_index(
+            candles, indicator_two, index - 1
         ):
             return True
 
--- tests/analysis/test_movement.py
+++ tests/analysis/test_movement.py
@@ -51,10 +51,10 @@ def fixture_mixed_candles_two():
 def fixture_indicator_candles():
     return [
         Candle(
-            open=130, high=150, low=120, close=120, volume=10, indicators={"EMA_10": 100}
+            open=130, high=100, low=120, close=120, volume=10, indicators={"EMA_10": 100}
         ),
         Candle(
-            open=120, high=140, low=110, close=120, volume=10, indicators={"EMA_10": 100}
+            open=120, high=110, low=110, close=120, volume=10, indicators={"EMA_10": 100}
         ),
         Candle(
             open=110, high=150, low=120, close=130, volume=10, indicators={"EMA_10": 100}
@@ -64,6 +64,28 @@ def fixture_indicator_candles():
         ),
         Candle(
             open=115, high=140, low=110, close=120, volume=10, indicators={"EMA_10": 140}
+        ),
+    ]
+
+
+@pytest.fixture(name="indicator_candles_partial")
+def fixture_indicator_candles_partial():
+    return [
+        Candle(open=130, high=150, low=120, close=120, volume=10, indicators={}),
+        Candle(open=120, high=140, low=110, close=120, volume=10, indicators={}),
+        Candle(
+            open=110, high=150, low=120, close=130, volume=10, indicators={"EMA_10": 100}
+        ),
+        Candle(
+            open=150, high=120, low=90, close=115, volume=10, indicators={"EMA_10": 110}
+        ),
+        Candle(
+            open=115,
+            high=140,
+            low=110,
+            close=120,
+            volume=10,
+            indicators={"EMA_10": 140, "SMA_10": 100},
         ),
     ]
 
@@ -82,6 +104,10 @@ def test_positive_list():
     )
 
 
+def test_positive_list_missing():
+    assert not movement.positive([])
+
+
 def test_negative():
     assert movement.negative(Candle(open=100, high=120, low=90, close=90, volume=10))
 
@@ -96,8 +122,28 @@ def test_negative_list():
     )
 
 
+def test_negative_list_missing():
+    assert not movement.negative([], position=0)
+
+
+def test_negative_list_empty():
+    assert not movement.negative([])
+
+
 def test_value_range(rising_candles):
     assert movement.value_range(rising_candles, "close") == 40
+
+
+def test_value_range_missing():
+    assert movement.value_range([], "close") is None
+
+
+def test_value_range_partial(indicator_candles_partial):
+    assert movement.value_range(indicator_candles_partial, "EMA_10") == 40
+
+
+def test_value_range_prtial_missing(indicator_candles_partial):
+    assert movement.value_range(indicator_candles_partial, "SMA_10") is None
 
 
 def test_basic_rising(rising_candles):
@@ -105,7 +151,23 @@ def test_basic_rising(rising_candles):
 
 
 def test_basic_rising_false(fallling_candles):
-    assert not movement.rising(fallling_candles, "close")
+    assert movement.rising(fallling_candles, "close") is False
+
+
+def test_basic_rising_missing():
+    assert movement.rising([], "close") is False
+
+
+def test_basic_rising_partial(indicator_candles_partial):
+    assert movement.rising(indicator_candles_partial, "EMA_10") is True
+
+
+def test_basic_rising_partial_missing(indicator_candles_partial):
+    assert movement.rising(indicator_candles_partial, "SMA_10") is False
+
+
+def test_basic_rising_length(rising_candles):
+    assert movement.rising(rising_candles, "close", 100) is True
 
 
 def test_basic_falling(fallling_candles):
@@ -116,12 +178,44 @@ def test_basic_falling_false(rising_candles):
     assert not movement.falling(rising_candles, "close")
 
 
+def test_basic_falling_missing():
+    assert movement.falling([], "close") is False
+
+
+def test_basic_falling_partial(indicator_candles_partial):
+    assert movement.falling(indicator_candles_partial, "EMA_10") is False
+
+
+def test_basic_falling_partial_missing(indicator_candles_partial):
+    assert movement.falling(indicator_candles_partial, "SMA_10") is False
+
+
+def test_basic_falling_length(fallling_candles):
+    assert movement.falling(fallling_candles, "close", 100) is True
+
+
 def test_mean_rising(mixed_candles):
-    assert movement.mean_rising(mixed_candles, "close")
+    assert movement.mean_rising(mixed_candles, "close") is True
 
 
 def test_mean_rising_false(mixed_candles_two):
-    assert not movement.mean_rising(mixed_candles_two, "close")
+    assert movement.mean_rising(mixed_candles_two, "close") is False
+
+
+def test_mean_rising_empty():
+    assert movement.mean_rising([], "close") is False
+
+
+def test_mean_rising_partial(indicator_candles_partial):
+    assert movement.mean_rising(indicator_candles_partial, "EMA_10") is True
+
+
+def test_mean_rising_partial_missing(indicator_candles_partial):
+    assert movement.mean_rising(indicator_candles_partial, "SMA_10") is False
+
+
+def test_mean_rising_length(mixed_candles):
+    assert movement.mean_rising(mixed_candles, "close", 100) is True
 
 
 def test_mean_falling(mixed_candles_two):
@@ -132,73 +226,157 @@ def test_mean_falling_false(mixed_candles):
     assert not movement.mean_falling(mixed_candles, "close")
 
 
-def test_highest(mixed_candles):
-    assert movement.highest(mixed_candles, "close") == 150
+def test_highest(indicator_candles):
+    assert movement.highest(indicator_candles, "close") == 130
 
 
-def test_highest_two(mixed_candles):
-    assert movement.highest(mixed_candles, "low") == 120
+def test_highest_two(indicator_candles):
+    assert movement.highest(indicator_candles, "low") == 120
 
 
-def test_lowest(mixed_candles_two):
-    assert movement.lowest(mixed_candles_two, "close") == 115
+def test_highest_missing():
+    assert movement.highest([], "close") is False
 
 
-def test_lowest_two(mixed_candles_two):
-    assert movement.lowest(mixed_candles_two, "low") == 90
+def test_highest_partial(indicator_candles_partial):
+    assert movement.highest(indicator_candles_partial, "EMA_10") == 140
 
 
-def test_highestbar(mixed_candles):
-    assert movement.highestbar(mixed_candles, "close") == 2
+def test_highest_partial_missing(indicator_candles_partial):
+    assert movement.highest(indicator_candles_partial, "SMA_10") == 100
 
 
-def test_highestbar_two(fallling_candles):
-    assert movement.highestbar(fallling_candles, "low") == 4
+def test_highest_length(indicator_candles):
+    assert movement.highest(indicator_candles, "open", 200) == 150
 
 
-def test_lowestbars(mixed_candles_two):
-    assert movement.lowestbar(mixed_candles_two, "open") == 2
+def test_lowest(indicator_candles):
+    assert movement.lowest(indicator_candles, "close") == 115
 
 
-def test_lowestbars_two(mixed_candles_two):
-    assert movement.lowestbar(mixed_candles_two, "close") == 1
+def test_lowest_two(indicator_candles):
+    assert movement.lowest(indicator_candles, "high") == 110
 
 
-def test_lowestbars_two_length(mixed_candles_two):
-    assert movement.lowestbar(mixed_candles_two, "close", length=100) == 1
+def test_lowest_missing():
+    assert movement.lowest([], "low") is False
 
 
-def test_cross(indicator_candles):
-    assert movement.cross(indicator_candles, "EMA_10", "close")
+def test_lowest_partial(indicator_candles_partial):
+    assert movement.lowest(indicator_candles_partial, "EMA_10") == 100
 
 
-def test_cross_length(indicator_candles):
-    assert movement.cross(indicator_candles, "EMA_10", "close", length=100)
+def test_lowest_partial_missing(indicator_candles_partial):
+    assert movement.lowest(indicator_candles_partial, "SMA_10") == 100
 
 
-def test_cross_any_direction(indicator_candles):
-    assert movement.cross(indicator_candles, "close", "EMA_10")
+def test_lowest_length(indicator_candles):
+    assert movement.lowest(indicator_candles, "high", 100) == 100
 
 
-def test_crossover(indicator_candles):
-    assert movement.crossover(indicator_candles, "EMA_10", "close")
+def test_highestbar(indicator_candles):
+    assert movement.highestbar(indicator_candles, "close") == 2
 
 
-def test_crossover_length(indicator_candles):
-    assert movement.crossover(indicator_candles, "EMA_10", "close", length=10)
+def test_highestbar_two(indicator_candles):
+    assert movement.highestbar(indicator_candles, "low") == 2
 
 
-def test_crossover_invalid(indicator_candles):
-    assert movement.crossover(indicator_candles, "close", "EMA_10") is False
+def test_highestbar_missing():
+    assert movement.highestbar([], "close") is None
 
 
-def test_crosunder(indicator_candles):
-    assert movement.crossunder(indicator_candles, "close", "EMA_10")
+def test_highestbar_partial(indicator_candles_partial):
+    assert movement.highestbar(indicator_candles_partial, "EMA_10") == 0
 
 
-def test_crosunder_length(indicator_candles):
-    assert movement.crossunder(indicator_candles, "close", "EMA_10", length=10)
+def test_highestbar_partial_missing(indicator_candles_partial):
+    assert movement.highestbar(indicator_candles_partial, "SMA_10") == 0
 
 
-def test_crosunder_invalid(indicator_candles):
-    assert movement.crossunder(indicator_candles, "EMA_10", "close") is False
+def test_highestbar_length(indicator_candles):
+    assert movement.highestbar(indicator_candles, "low", 100) == 2
+
+
+def test_lowestbars(indicator_candles):
+    assert movement.lowestbar(indicator_candles, "open") == 2
+
+
+def test_lowestbars_two(indicator_candles):
+    assert movement.lowestbar(indicator_candles, "close") == 1
+
+
+def test_lowestbars_missing():
+    assert movement.lowestbar([], "open") is None
+
+
+def test_lowestbars_partial(indicator_candles_partial):
+    assert movement.lowestbar(indicator_candles_partial, "EMA_10") == 2
+
+
+def test_lowestbars_partial_missing(indicator_candles_partial):
+    assert movement.lowestbar(indicator_candles_partial, "SMA_10") == 0
+
+
+def test_lowestbars_length(indicator_candles):
+    assert movement.lowestbar(indicator_candles, "open", length=100) == 2
+
+
+class TestCrossMethods:
+    def test_cross(self, indicator_candles):
+        assert movement.cross(indicator_candles, "EMA_10", "close") is True
+
+    def test_cross_no_candles(self):
+        assert movement.cross([], "EMA_10", "close") is False
+
+    def test_cross_length(self, indicator_candles):
+        assert movement.cross(indicator_candles, "EMA_10", "close", length=100) is True
+
+    def test_cross_any_direction(self, indicator_candles):
+        assert movement.cross(indicator_candles, "close", "EMA_10") is True
+
+    def test_crossover(self, indicator_candles):
+        assert movement.crossover(indicator_candles, "EMA_10", "close") is True
+
+    def test_crossover_two(self, indicator_candles):
+        assert movement.crossover(indicator_candles, "EMA_10", "low") is False
+
+    def test_crossover_no_candles(self):
+        assert movement.crossover([], "EMA_10", "close") is False
+
+    def test_crossover_partial(self, indicator_candles_partial):
+        assert (
+            movement.crossover(indicator_candles_partial, "volume", "EMA_10", 5) is False
+        )
+
+    def test_crossover_partial_missing(self, indicator_candles_partial):
+        assert (
+            movement.crossover(indicator_candles_partial, "volume", "SMA_10", 5) is False
+        )
+
+    def test_crossover_length(self, indicator_candles):
+        assert movement.crossover(indicator_candles, "EMA_10", "close", length=10) is True
+
+    def test_crossunder(self, indicator_candles):
+        assert movement.crossunder(indicator_candles, "close", "EMA_10") is True
+
+    def test_crossunder_two(self, indicator_candles):
+        assert movement.crossunder(indicator_candles, "low", "EMA_10") is False
+
+    def test_crossunder_no_candles(self):
+        assert movement.crossunder([], "close", "EMA_10") is False
+
+    def test_crossunder_partial(self, indicator_candles_partial):
+        assert (
+            movement.crossunder(indicator_candles_partial, "volume", "EMA_10", 5) is False
+        )
+
+    def test_crossunder_partial_missing(self, indicator_candles_partial):
+        assert (
+            movement.crossunder(indicator_candles_partial, "volume", "SMA_10", 5) is False
+        )
+
+    def test_crossunder_length(self, indicator_candles):
+        assert (
+            movement.crossunder(indicator_candles, "close", "EMA_10", length=10) is True
+        )
