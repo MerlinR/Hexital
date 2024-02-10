4ffbece3aa392e838c63006bccc45efa5590385d
Merlin Roe <merlin.roe@hotmail.co.uk>
2024-02-10 15:13:51 +0000

    Basic tidying on Amoprh

--- hexital/indicators/amorph.py
+++ hexital/indicators/amorph.py
@@ -1,10 +1,8 @@
-import importlib
 import inspect
 from copy import deepcopy
 from typing import Callable, Optional
 
 from hexital.core.indicator import Indicator
-from hexital.exceptions import InvalidAnalysis
 
 
 class Amorph(Indicator):
@@ -23,23 +21,9 @@ class Amorph(Indicator):
     _analysis_method: Callable
     _analysis_kwargs: dict
 
-    def __init__(self, analysis: str | Callable, args: Optional[dict] = None, **kwargs):
-        if isinstance(analysis, str):
-            pattern_module = importlib.import_module("hexital.analysis.patterns")
-            movement_module = importlib.import_module("hexital.analysis.movement")
-            pattern_method = getattr(pattern_module, analysis, None)
-            analysis_func = getattr(movement_module, analysis, pattern_method)
-
-            if analysis_func is not None:
-                self._analysis_method = analysis_func
-
-        elif callable(analysis):
-            self._analysis_method = analysis
-
-        if not hasattr(self, "_analysis_method"):
-            raise InvalidAnalysis(f"The given analysis [{analysis}] is invalid")
-
-        self._analysis_kwargs, kwargs = self._seperate_indicator_attributes(kwargs)
+    def __init__(self, analysis: Callable, args: Optional[dict] = None, **kwargs):
+        self._analysis_method = analysis
+        self._analysis_kwargs, kwargs = self._separate_indicator_attributes(kwargs)
 
         if isinstance(args, dict):
             self._analysis_kwargs.update(args)
@@ -49,10 +33,9 @@ class Amorph(Indicator):
     @property
     def settings(self) -> dict:
         """Returns a dict format of how this indicator can be generated"""
-        settings = self.__dict__
         output = {"analysis": self._analysis_method.__name__}
 
-        for name, value in settings.items():
+        for name, value in self.__dict__.items():
             if name == "candles":
                 continue
             if name == "timeframe_fill" and self.timeframe is None:
@@ -63,7 +46,7 @@ class Amorph(Indicator):
         return output
 
     @staticmethod
-    def _seperate_indicator_attributes(kwargs: dict) -> tuple[dict, dict]:
+    def _separate_indicator_attributes(kwargs: dict) -> tuple[dict, dict]:
         indicator_attr = inspect.getmembers(Indicator)[1][1].keys()
         analysis_args = {}
         for argum in list(kwargs.keys()):
@@ -74,8 +57,8 @@ class Amorph(Indicator):
 
     def _generate_name(self) -> str:
         name = self._analysis_method.__name__
-        if self._analysis_kwargs.get("length"):
-            name += f"_{self._analysis_kwargs['length']}"
+        length = self._analysis_kwargs.get("length")
+        name = name + f"_{length}" if length else name
         return name
 
     def _calculate_reading(self, index: int) -> float | dict | None:
